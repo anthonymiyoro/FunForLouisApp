10e25a93d2d5d5ceeb86868f3726472d02b40d12
anthonymiyoro <amiyoro@gmail.com>
2015-08-03 15:39:41 +0300

    Added build.gradle files

---
+++ app/src/main/java/com/example/tony/funforlouis/dummy/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.tony.funforlouis.dummy;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.crashlytics.android.Crashlytics;
+import com.google.android.youtube.player.YouTubeApiServiceUtil;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+
+import io.fabric.sdk.android.Fabric;
+
+public class MainActivity  {
+
+    // Checks if the youtube app is in the phone
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
+        setContentView(R.layout.activity_main);
+
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        //Check for any issues
+        final YouTubeInitializationResult result = YouTubeApiServiceUtil.isYouTubeApiServiceAvailable(this);
+
+        if (result != YouTubeInitializationResult.SUCCESS) {
+            //If there are any issues we can show an error dialog.
+            result.getErrorDialog(this, 0).show();
+        }
+    }
+
+
+}
