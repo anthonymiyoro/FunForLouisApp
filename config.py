64aea7fca83a4fbd29ff0732f9bda81f8385e8cc
Anthony Miyoro <amiyoro@gmail.com>
2017-11-21 01:14:56 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 CONFIG = {
-     'FACEBOOK_TOKEN': 'EAAHUZAIvkVjwBAP2ZA99jI1U357BZBkjQ4UpSZBojNnZB1ZCn1Hl2KR6S93kjWZAzSbuLTwuX1h98kAZAIjs5rYpBcwKxdLF1mFlnqC2GIMSoZAioyW4orexO9tCCHvdCiPv0dmbpMiiDkrbn45WWePx8TkHDYZCnnWEO7sZCoQTIKTAwZDZD',
+     'FACEBOOK_TOKEN': 'FACEBOOK_TOKEN_HERE',
      'VERIFY_TOKEN': 'my_verify_token',
-     'SERVER_URL': 'https://hotel-example-bot.herokuapp.com/'
+     'SERVER_URL': 'SERVER_URL_HERE'
  }
+
